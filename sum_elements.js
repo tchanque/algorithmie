f026eac6d880ffb0e9e82c5483116c39c5d2e7ba
tchanque <audevie.thomas@gmail.com>
2024-04-17 16:20:06 +0200

    add operations:

--- sum_elements.js
+++ sum_elements.js
@@ -3,7 +3,9 @@ let parsedData = [10, 15, 3, 7];
 
 // EXERCICE 1
 
-const checkSum = (array, k) => {
+let comparison1 = 0;
+
+const checkSumN2Complexity = (array, k) => {
     // parcourir la liste deux a deux
     // faire somme des elements et comparer a k
     // si somme == k alors on retourne true
@@ -11,7 +13,7 @@ const checkSum = (array, k) => {
     for (let i=0; i < array.length; i++) {
         
         for (let j=i+1; j < array.length; j++) {
-
+            comparison1++
 
             let sum = array[i] + array[j]
             console.log(`Summing ${array[i]} with ${array[j]} equals ${sum}`)
@@ -26,15 +28,18 @@ const checkSum = (array, k) => {
 
 }
 
-// console.log(checkSum(parsedData, 150));
+let response = checkSumN2Complexity(parsedData, 0)
+console.log(`Checking if sum exists returns: ${response} after ${comparison1} operations`);
 
 // EXERCICE 3 
 
+let comparison2 = 0;
 
 const checkSumNComplexity = (array, k) => {
     let alreadyChecked = new Set();
     
     for(let i= 0; i < array.length; i++) {
+        comparison2++;
         const complement = k - array[i];
         if(alreadyChecked.has(complement)){
             return true
@@ -44,4 +49,6 @@ const checkSumNComplexity = (array, k) => {
     return false;
 }
 
-console.log(checkSumNComplexity(parsedData, 0));
+response = checkSumNComplexity(parsedData, 0)
+ 
+console.log(`Checking if sum exists returns: ${response} after ${comparison2} operations`);
--- west_side.js
+++ west_side.js
@@ -3,11 +3,14 @@ array = [3, 7, 8, 3, 6, 1];
 
 // EXERCICE 2
 
+let countOperations = 0;
+
 let countWestOrientatedNComplexity = (array) => {
     let buildingCount = 1;
     let biggestBuilding = array.slice(-1)
     
     for (let i=array.length-2; i >= 0; i--) {
+        countOperations++;
 
         if(biggestBuilding < array[i]) {
             biggestBuilding = array[i]
@@ -18,9 +21,11 @@ let countWestOrientatedNComplexity = (array) => {
     return buildingCount;
 }
 
-console.log(`With N complexity: The number of buildings with a view on the West is ${countWestOrientatedNComplexity(array)}`);
+console.log(`With N complexity: The number of buildings with a view on the West is ${countWestOrientatedNComplexity(array)} after ${countOperations} operations`);
 
 // EXERCICE 4
+
+countOperations = 0;
 
 let countWestOrientatedN2Complexity = (array) => {
     let noViewBuildingCount = 0;
@@ -30,6 +35,7 @@ let countWestOrientatedN2Complexity = (array) => {
 
         for (let j=i+1; j < array.length; j++) { // check one by one the buildings on its right (those further to the west)
             // 1er tour j vaut 3 donc valeur = 8
+            countOperations++;
             if (array[i] < array[j]) { // if a building on the west is higher than our building, then we break the loop since our building doesn't have a view on the west
                 noViewBuildingCount++
                 break;
@@ -41,4 +47,4 @@ let countWestOrientatedN2Complexity = (array) => {
     return (array.length - noViewBuildingCount); // return the number of buildins with a view on west
 }
 
-console.log(`With N2 complexity: The number of buildings with a view on the West is ${countWestOrientatedN2Complexity(array)}`);
+console.log(`With N2 complexity: The number of buildings with a view on the West is ${countWestOrientatedN2Complexity(array)} after ${countOperations} operations`);
